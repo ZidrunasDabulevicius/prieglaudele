91394ab622b38847e275ed0c2e856e9e4b14e72b
ZidrunasDabulevicius <100692786+ZidrunasDabulevicius@users.noreply.github.com>
2022-04-25 11:40:02 +0300

    Add files via upload

---
+++ pages/registracija.php
@@ -0,0 +1,31 @@
+<?php
+if (isset($_COOKIE["vartotojas"])){
+    header("Location: index.php");
+}
+?>
+
+<!DOCTYPE html>
+
+
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        
+        <form action="peradresavimas.php" method="POST">
+
+          <label for="vartotojo_vardas">Iveskite vartotojo varda:</label><br>
+          <input type="text" name="vartotojo_vardas" required><br><br>
+          <label for="slaptazodis">Iveskite slaptazodi:</label><br>
+          <input type="text" name="slaptazodis" required><br><br>
+          <label for="elpastas">Iveskite el pasta:</label><br>
+          <input type="text" name="elpastas" required><br><br>
+          <input type="hidden" name="slapukas">
+          <input type="submit" value="Registruotis">
+      </form>
+
+    </body>
+</html>
